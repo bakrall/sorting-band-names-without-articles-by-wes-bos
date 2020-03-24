33cc1b433a81c63699d956ecf649267087793a2d
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-03-24 16:19:55 +0100

    Wes Bos's solution

--- index.js
+++ index.js
@@ -2,35 +2,24 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 
 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const bandsList = document.querySelector('.bands');
-
-function sortBands() {
-	let orderedBands = bands.sort((a, b) => {
-		a = formatBand(a);
-		b = formatBand(b);
-
-		if (a < b) return -1;
-		if (a > b) return 1;
-		return 0;
-
-	});	
-
-	return orderedBands;
+function strip(bandName) {
+	return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
-function formatBand(el) {
-	el = el.toLowerCase();
-	el = el.match(/^(the|a|an)\b\s?/) ? el.replace(/^(the|a|an)\b\s?/, '') : el;
+//this will be written in one line
+	// if (a > b) {
+	// 	return 1;
+	// } else {
+	// 	return -1;
+	// }
 
-	return el;
-}
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-function renderList() {
-	const orderedBands = sortBands();
+document.querySelector('.bands').innerHTML = 
+	sortedBands
+		.map(band => `<li>${band}</li>`)
+		.join('');
 
-	bandsList.innerHTML = orderedBands.map((band, i) => {
-		return `<li>${band}</li>`;
-	}).join('');
-}
-
-renderList();
+//if we put to innerHTML something other than a string, a method 'toString()' will automatically be called on the thing passed
+//so if we pass an array of bands, this array will be converted to one big string with bands seperated with comma
+//to avoid that (commas would be rendered in the list), we use join(''), which converts array to one big string
